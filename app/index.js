919c81c8b0ae655abd455a383198e7b09c0aee8c
bexi <trreitmaier@msn.com>
2018-02-03 11:52:29 +0100

    now have nicknames

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var nicknames = {};
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -11,7 +12,30 @@ http.listen(3000, function(){
 });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
+  /*socket.on('chat message', function(msg){
     io.emit('chat message', msg);
-  });
+  });*/
+
+  socket.on('user message', function (msg) {
+   socket.broadcast.emit('user message', socket.nickname, msg);
+ });
+
+ socket.on('nickname', function (nick, fn) {
+   if (nicknames[nick]) {
+     fn(true);
+   } else {
+     fn(false);
+     nicknames[nick] = socket.nickname = nick;
+     socket.broadcast.emit('announcement', nick + ' connected');
+     io.sockets.emit('nicknames', nicknames);
+   }
+ });
+
+ socket.on('disconnect', function () {
+   if (!socket.nickname) return;
+
+   delete nicknames[socket.nickname];
+   socket.broadcast.emit('announcement', socket.nickname + ' disconnected');
+   socket.broadcast.emit('nicknames', nicknames);
+ });
 });
