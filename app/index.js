9507d130b95140ce2b2389074677cf2d7c02ac26
bexi <trreitmaier@msn.com>
2018-02-03 16:10:13 +0100

    changes js to files

--- app/index.js
+++ app/index.js
@@ -9,7 +9,7 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-app.use(express.static('js'));
+app.use(express.static('files'));
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
